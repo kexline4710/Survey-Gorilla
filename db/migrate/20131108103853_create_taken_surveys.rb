b4d10e0399be504108a2e3417bf64dcb0e8afcc7
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-11-08 14:40:34 -0600

    fixed the seeds table

--- db/migrate/20131108103853_create_taken_surveys.rb
+++ db/migrate/20131108103853_create_taken_surveys.rb
@@ -1,4 +1,11 @@
-class CreateTakenSurveys < ActiveRecord::Migration   def change     create_table
-:taken_surveys do |t|       t.integer :user_id       t.boolean :completion,
-default: false       t.integer :survey_id       t.timestamps     end
-add_index(:taken_surveys, [:user_id, :survey_id], :unique => true)   end end
+class CreateTakenSurveys < ActiveRecord::Migration   
+  def change     
+    create_table :taken_surveys do |t|
+      t.integer :user_id       
+      t.boolean :completion, default: false       
+      t.integer :survey_id       
+      t.timestamps     
+    end
+    add_index(:taken_surveys, [:user_id, :survey_id], :unique => true)   
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+clay = User.create(username:"Clay", email: "clay@dbc.com", password: "password")
+jane = User.create(username:"Jane", email: "jane@dbc.com", password: "password")
+
+clay_survey = Survey.create(title: "Clay's First Survey")
+clay.surveys << clay_survey
+clay_survey_q1 = Question.create(q_content: "Why are we here?")
+clay_survey.questions << clay_survey_q1
+clay_survey_q1_a1 = PossibleAnswer.create(a_content: "For pleasure.")
+clay_survey_q1_a2 = PossibleAnswer.create(a_content: "For pain.")
+clay_survey_q1.possible_answers << clay_survey_q1_a1
+clay_survey_q1.possible_answers << clay_survey_q1_a2
+clay_survey_q2 = Question.create(q_content: "What will Alex draw next?")
+clay_survey.questions << clay_survey_q2
+clay_survey_q2_a1 = PossibleAnswer.create(a_content: "An olitaur.")
+clay_survey_q2_a2 = PossibleAnswer.create(a_content: "Himself riding an olitaur.")
+clay_survey_q2.possible_answers << clay_survey_q2_a1
+clay_survey_q2.possible_answers << clay_survey_q2_a2
+jane.taken_surveys << TakenSurvey.create(survey_id: clay_survey.id)
+jane.answers_users << AnswersUser.create(possible_answer_id: clay_survey_q1_a1.id)
+jane.answers_users << AnswersUser.create(possible_answer_id: clay_survey_q2_a2.id)
+
+
+
