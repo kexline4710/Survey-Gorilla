16c4f43c2964261551d11a56c9d707e98f8a250f
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-09 15:27:31 -0600

    edited the results page and added some associations to the model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :survey
   has_many :possible_answers
+  has_many :answers_users, through: :possible_answers
 end
