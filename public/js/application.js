9bf69a6d9961545af02dcc45d86c60b1689ded70
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-11-09 11:28:36 -0600

    fixed 'finish and review' functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-$(".forms").hide()
-$("ul li a").click(function(){
-  var idToShow = $(this).attr("href");
-  $(idToShow).show().siblings(".forms").hide();
-  return false;
-  });
+// $(".forms").hide()
+// $("ul li a").click(function(){
+//   var idToShow = $(this).attr("href");
+//   $(idToShow).show().siblings(".forms").hide();
+//   return false;
+//   });
 });
