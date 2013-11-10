1295fa48007db1e205a6e36a752e9498edca3ba9
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-09 19:07:59 -0600

    taker conroller additions

--- app/controllers/take_survey.rb
+++ app/controllers/take_survey.rb
@@ -1,6 +1,6 @@
 post '/surveys/:survey_id/taken_surveys' do
   current_survey = Survey.find(params[:survey_id])
-  TakenSurvey.create(survey_id: current_survey.id, user_id: 2)
+  TakenSurvey.create(survey_id: current_survey.id, user_id: current_user.id)
   redirect to "/surveys/#{current_survey.id}/questions/1"
 end
 
@@ -13,7 +13,7 @@ end
 post '/surveys/:survey_id/questions/:question_id/answers_users' do
   survey = Survey.find(params[:survey_id])
   question  = Question.find(params[:question_id])
-  AnswersUser.create(user_id: 2, possible_answer_id: params[:possible_answer_id])
+  AnswersUser.create(user_id: current_user.id, possible_answer_id: params[:possible_answer_id])
   next_question = next_question(survey)
   if next_question != nil
     redirect to "/surveys/#{survey.id}/questions/#{next_question.id}"
@@ -25,8 +25,8 @@ end
 get '/surveys/:survey_id/results' do
   @survey = Survey.find(params[:survey_id])
   @questions = @survey.questions
-  @taken_survey = TakenSurvey.find_by_user_id_and_survey_id(2,1)
-  @users_responses = @survey.answers_users.where("user_id = ?", 2)
+  @taken_survey = TakenSurvey.find_by_user_id_and_survey_id(current_user.id,1)
+  @users_responses = @survey.answers_users.where("user_id = ?", current_user.id)
   @responses = AnswersUser.all
   erb :results
 end
