63163cc5384b36b51cc809a3041b97a56a6cb434
Apprentice <apprentice@Alexs-Mac-mini.local>
2013-11-09 23:39:55 -0600

    added display of login errors, new bug -- survey questions not advancing

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -15,23 +15,25 @@ get '/profile' do
 end
 
 post '/login' do
+  
   if User.find_by_username(params[:username]) 
     user = User.find_by_username(params[:username])
     if user.password == params[:password]
       session[:user_id] = User.find_by_username(params[:username]).id
       redirect '/profile'
-      @user = user
-    else
-      @errors_message = "Sorry username or password are incorrect"
-      redirect '/login_page'
     end
   end
-  @error_message = "Sorry username does not exist"
-  redirect '/login_page'
+    session[:error] = "Sorry that username or password doesn't exits"
+    redirect '/login_page'
 end
 
 post '/users/new' do
   user = User.create(params[:new])
-  session[:user_id] = user.id
-  redirect '/profile'
+  if user.id
+    session[:user_id] = user.id
+    redirect '/profile'
+  else
+    session[:error] = user.errors.full_messages
+    redirect '/login_page'
+  end
 end
